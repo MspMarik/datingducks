7bc18a4d33f690aad76bff4d3e38b53e4aae9f19
MspMarik <msp3472@gmail.com>
2022-05-01 18:53:33 -0400

    added Mark's face and font awesome icons

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faHeartCrack } from "@fortawesome/free-solid-svg-icons";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import logo from "../logo.svg";
+import markFace from "../testImg/mark-face.JPEG";
 import "../App.css";
 
 const Home = () => {
@@ -26,17 +29,17 @@ const Home = () => {
         return (
             <div className="container align-self-center" style={{ width: "40rem" }}>
                 <Card className="card-shadow">
-                    <Card.Img variant="top" src={logo} style={{ backgroundColor: "black" }} />
+                    <Card.Img variant="top" src={markFace} style={{ backgroundColor: "black" }} />
                     <Card.Body>
                         <Card.Title>Card Title</Card.Title>
                         <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
                     </Card.Body>
                 </Card>
                 <Button className="m-4" variant="success">
-                    Like
+                    <FontAwesomeIcon icon={faHeart} />
                 </Button>
                 <Button className="m-4" variant="danger">
-                    Dislike
+                    <FontAwesomeIcon icon={faHeartCrack} />
                 </Button>
             </div>
         );
